bd6820f6f7bf177d6f1f2c003cb7acb214166db1
Noob <aaziz9642@gmail.com>
2021-09-12 00:06:37 +0600

    edit navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,8 +5,9 @@ function Header()
     return(
         <div>
             <header> 
-            
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+               
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark pt-0 pb-0">
+                    <div className="container">
                     <a class="navbar-brand" href="#">ProShop</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -36,11 +37,12 @@ function Header()
                             </li>
                         </ul>
                         <form class="form-inline my-2 my-lg-0">
-                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                            <button class ="btn btn-outline-success my-2 my-sm-0" type ="submit">Search</button>
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart <span class="badge badge-secondary"> 5</span></button>
                         </form>
                     </div>
+                </div>
                 </nav>
+                
 
 
             </header>
