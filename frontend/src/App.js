8a7cf9e9f01e3d686c284f95dd292607702783f3
Noob <aaziz9642@gmail.com>
2021-09-11 23:46:59 +0600

    add navbar and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div>
+         <Header/>
+         <main>
+           <div className = "container">
+          <h1>Welcome to proshop</h1>
+             </div>
+           
+          </main>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+
+            <div className="container">
+                   <div className = "row ">
+                    <div className= "col text-center">
+                        <p>Copyright &copy; ProShop</p>
+                    </div>
+                       
+                   </div>
+            </div>
+
+        </footer>
+    )
+}
+export default Footer;
