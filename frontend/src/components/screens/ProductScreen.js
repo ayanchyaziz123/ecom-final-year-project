f0c3c1076f522ff515a70039bc4e13fdc9b695bd
Noob <aaziz9642@gmail.com>
2021-09-13 22:57:10 +0600

    added product details page

--- frontend/src/components/screens/ProductScreen.js
+++ frontend/src/components/screens/ProductScreen.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import products from '../../products';
-import Product from '../../components/Product';
-function ProductScreen() {
+import Rating from '../../components/Rating';
+function ProductScreen({match}) {
+    const product = products.find((p) => p._id == match.params.id)
     return (
         <div>
-            <h1>Latest Products</h1>
+            <Link to='/' className="btn btn-light my-3">Go Back</Link>
             <div className="row">
-                product
+                <div className="col-sm-6">
+                     <img src={product.image} className="img-fluid"/>
+                </div>
+                <div className="col-sm-3">
+
+                    <ul class="list-group shadow">
+                        <li class="list-group-item">{product.name}</li>
+                        <li class="list-group-item"><Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} /></li>
+                        <li class="list-group-item">Price ${product.price}</li>
+                        <li class="list-group-item">Description : {product.description}</li>
+                    </ul>
+
+                </div>
+
+                <div className="col-sm-3">
+                    <ul class="list-group shadow">
+                        <li class="list-group-item">Price ${product.price}</li>
+                        <li class="list-group-item">Status : {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}</li>
+                        <li class="list-group-item"><button className="btn-block btn-dark" disabled={product.countInStock == 0} type="button">Add to cart</button></li>
+                    </ul>
+
+                </div>
+
             </div>
         </div>
     )
