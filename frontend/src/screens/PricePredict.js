d53fd6687c95008a1b2d1ad6e287c3eaf3df9e7e
Noob <aaziz9642@gmail.com>
2021-10-06 00:02:21 +0600

    added

--- frontend/src/screens/PricePredict.js
+++ frontend/src/screens/PricePredict.js
@@ -2,42 +2,42 @@ import React, { useState } from 'react';
 
 function PricePredict(){
 
-    const [inputs, setInputs] = useState({});
-
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({ ...values, [name]: value }))
-    }
-
-    const handleSubmit = (event) => {
-        if (event.name == "Samsung")
-        {
-            console.log("samsung");
-        }
-        event.preventDefault();
-        console.log(inputs);
-    }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Brand Name:
-                <select name="brand_name" onChange={handleChange} class="form-control">
-                    <option value="Samsung">Samsung</option>
-                    <option value="IPhone">Iphone</option>
-                    <option value="Oppo">Oppo</option>
-                </select>
-            </label>
+        <form>
+            <label for="exampleFormControlSelect1">Brand Name</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <option>Iphone</option>
+                <option>Samsung</option>
+                <option>Oppo</option>
+            </select>
+            <label for="exampleFormControlSelect1">RAW</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <label for="exampleFormControlSelect1">ROM</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
+            <label for="exampleFormControlSelect1">Camera</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </select>
             <br></br>
-            <label>Brand Name:
-                <select name="ram" onChange={handleChange} class="form-control">
-                    <option value="2">2 GB</option>
-                    <option value="4">4 GB</option>
-                    <option value="6">6 GB</option>
-                </select>
-            </label>
-            <br></br>
-            <input className="btn btn-secondary" type="submit" />
+            <button className="btn btn-dark">Predict</button>
+            
         </form>
     )
 }
