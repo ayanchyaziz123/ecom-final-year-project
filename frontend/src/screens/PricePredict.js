d21fdd885b3b768935dc29171c5a6761c4b88e9c
Noob <aaziz9642@gmail.com>
2021-10-06 18:36:17 +0600

    new

--- frontend/src/screens/PricePredict.js
+++ frontend/src/screens/PricePredict.js
@@ -11,29 +11,29 @@ function PricePredict(){
                 <option>Samsung</option>
                 <option>Oppo</option>
             </select>
-            <label for="exampleFormControlSelect1">RAW</label>
+            <label for="exampleFormControlSelect1">RAM</label>
             <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+                <option>1 gb</option>
+                <option>2 gb</option>
+                <option>3 gb</option>
+                <option>4 gb</option>
+                <option>6 gb</option>
             </select>
             <label for="exampleFormControlSelect1">ROM</label>
             <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+                <option>16 gb</option>
+                <option>32 gb</option>
+                <option>64 gb</option>
+                <option>128 gb</option>
+                <option>256 gb</option>
             </select>
             <label for="exampleFormControlSelect1">Camera</label>
             <select class="form-control" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+                <option>10px</option>
+                <option>20px</option>
+                <option>30px</option>
+                <option>40px</option>
+                <option>50px</option>
             </select>
             <br></br>
             <button className="btn btn-dark">Predict</button>
